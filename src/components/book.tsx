c605485eb115de4b6a48b368db8539e6f80ea099
Cameron Sinclair <cameron.sinclair@Nology.local>
2019-05-13 16:37:26 +0100

    first commit for rotation

--- src/components/book.tsx
+++ src/components/book.tsx
@@ -24,25 +24,48 @@ class Book extends React.Component<IProps, IState> {
       ? this.props.book.volumeInfo.imageLinks.thumbnail
       : placeholder;
     return (
-      <div className={styles.bookinfo}>
-        <section className={styles.bookTitle}>
-          <div className={styles.title}>{this.props.book.volumeInfo.title}</div>
-          <div className={styles.picContainer}>
-            <img className={styles.picture} src={thumbnail} />
+      <React.Fragment>
+        <div className={styles.scene}>
+          <div className={styles.movie}>
+            <div className={styles.poster} />
+            <div className={styles.info}>
+              <header>
+                <h1>It's a Wonderful Life</h1>
+                <span className={styles.year}>1946</span>
+                <span className={styles.rating}>PG</span>
+                <span className={styles.duration}>130 minutes</span>
+              </header>
+              <p>
+                In Bedford Falls, New York on Christmas Eve, George Bailey is
+                deeply troubled. Prayers for his well-being from friends and
+                family reach Heaven. Clarence Odbody, Angel Second Class, is
+                assigned to visit Earth to save George, thereby earning his
+                wings. Franklin and Joseph, the head angels, review George's
+                life with Clarence.
+              </p>
+            </div>
           </div>
-        </section>
+        </div>
+      </React.Fragment>
+      // <div className={styles.bookinfo}>
+      //   <section className={styles.bookTitle}>
+      //     <div className={styles.title}>{this.props.book.volumeInfo.title}</div>
+      //     <div className={styles.picContainer}>
+      //       <img className={styles.picture} src={thumbnail} />
+      //     </div>
+      //   </section>
 
-        <section className={styles.description}>
-          {this.props.book.volumeInfo.description}
-        </section>
-        <section className={styles.properties}>
-          Genre: {genreType}
-          <br />
-          Length: {bookLength}
-          <br />
-          Date Published: {datePublished}
-        </section>
-      </div>
+      //   <section className={styles.description}>
+      //     {this.props.book.volumeInfo.description}
+      //   </section>
+      //   <section className={styles.properties}>
+      //     Genre: {genreType}
+      //     <br />
+      //     Length: {bookLength}
+      //     <br />
+      //     Date Published: {datePublished}
+      //   </section>
+      // </div>
     );
   }
 }
